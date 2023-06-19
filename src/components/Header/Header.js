6fe9229aa1c1e4f4c0a023f901135f0aa2617156
JAIJOTHI K <136154555+jai-jothi@users.noreply.github.com>
2023-06-19 16:37:49 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import classes from './Header.module.css';
 const Header =()=>{
 
     return (
-        <header className={classes.Header}>
+        <header className={classes.header}>
         <img src={logo} alt="logo" />
         <h1>Investment Calculator</h1>
       </header>
